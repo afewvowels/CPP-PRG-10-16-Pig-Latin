a5b7c51bf0a52e32b0007d2c0cbb1931f8c0de6d
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-05 12:12:06 -0500

    Tested and working.

--- CPP-PRG-10-16-Pig-Latin/main.cpp
+++ CPP-PRG-10-16-Pig-Latin/main.cpp
@@ -15,12 +15,95 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string *getString(string *);
+string *translateString(string *);
+void displayString(string *);
+
 int main()
 {
+    string *strUserString = nullptr;
+    strUserString = new string;
     
+    strUserString = getString(strUserString);
+    
+    strUserString = translateString(strUserString);
+    
+    displayString(strUserString);
     
     return 0;
+}
+
+string *getString(string *strStr)
+{
+    cout << "Please enter a string to translate into Pig Latin:\n";
+    getline(cin, *strStr);
+    
+    return strStr;
+}
+
+string *translateString(string *strStr)
+{
+    string *strTempWord = nullptr;
+    strTempWord = new string;
+    
+    string *strTempString = nullptr;
+    strTempString = new string;
+    
+    string *strReplacePhrase = nullptr;
+    strReplacePhrase = new string;
+    *strReplacePhrase = "AY";
+    
+    char *chrStringFull = nullptr;
+    chrStringFull = new char[strStr->length()];
+    
+    strcpy(chrStringFull, strStr->c_str());
+    
+    if(!isalpha(chrStringFull[strlen(chrStringFull) - 1]))
+        chrStringFull[strlen(chrStringFull) - 1] = '\0';
+    
+    for (int i = 0 ; i < strStr->length() ; i++)
+        chrStringFull[i] = toupper(chrStringFull[i]);
+    
+    strStr->clear();
+    strStr->assign(chrStringFull, strlen(chrStringFull));
+    
+    for (int i = (strlen(chrStringFull) - 1) ; i >= 0 ; i--)
+    {
+        if (chrStringFull[i] == ' ')
+        {
+            *strTempWord = strStr->substr(i + 1, strlen(chrStringFull));
+            
+            strTempWord->append(*strTempWord, 0, 1);
+            strTempWord->append(*strReplacePhrase);
+            strTempWord->erase(0, 1);
+            
+            strStr->resize(i);
+            
+            strTempString->insert(0, " " + *strTempWord);
+        }
+        else if (i == 0)
+        {
+            *strTempWord = strStr->substr(i, strlen(chrStringFull));
+            
+            strTempWord->append(*strTempWord, 0, 1);
+            strTempWord->append(*strReplacePhrase);
+            strTempWord->erase(0, 1);
+            
+            strTempString->insert(0, *strTempWord);
+            
+            strStr->resize(i);
+        }
+    }
+    
+    return strTempString;
+}
+
+void displayString(string *strStr)
+{
+    cout << "Your phrase translated into Pig Latin is:\n";
+    cout << *strStr << endl;
 }
